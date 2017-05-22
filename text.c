d5931da70ea578e64b1d908fe88f1dace09e873e
honey data <honeydatax@sapo.pt>
2017-05-22 22:28:44 +0000

    free code direct draw

---
+++ text.c
@@ -0,0 +1,34 @@
+#include "directX.h"
+
+
+
+
+
+int main()
+{
+    int fbfd = startX();
+char c[]="im love marina.";
+   
+   
+    if (fbfd == -1) exit(1);
+    if ((int)fbp == -1)exit(1);
+
+
+for (int xy=0;xy<vinfo.yres-20;xy=xy+20)gputs(xy,xy,0,0,255,c); 
+
+
+
+refresh(); 
+endX(fbfd);
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
