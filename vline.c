ffa6ba8eaddb0c52350e89690ec211040566b98b
honey data <honeydatax@sapo.pt>
2017-05-22 21:39:04 +0000

    free code direct draw linux

---
+++ vline.c
@@ -0,0 +1,34 @@
+#include "directX.h"
+
+
+
+
+
+int main()
+{
+    int fbfd = startX();
+
+   
+   
+    if (fbfd == -1) exit(1);
+    if ((int)fbp == -1)exit(1);
+
+
+vline(vinfo.xres/2,0,vinfo.yres,0,0,255);
+
+
+
+ 
+endX(fbfd);
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
