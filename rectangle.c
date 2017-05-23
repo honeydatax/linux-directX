9b7c4ee78a87b45e6ede29ec6aff05a3e08dd5c0
honey data <honeydatax@sapo.pt>
2017-05-23 06:48:17 +0000

    free code direct draw direct x

---
+++ rectangle.c
@@ -0,0 +1,33 @@
+#include "directX.h"
+
+
+
+
+
+int main()
+{
+    int fbfd = startX();
+char c[]="im love marina.";
+   
+   
+    if (fbfd == -1) exit(1);
+    if ((int)fbp == -1)exit(1);
+
+
+for(int xy=0;xy<vinfo.xres-20;xy=xy+20)rectangle(xy,xy,xy+20,xy+20,0,0,255);
+
+
+refresh(); 
+endX(fbfd);
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
