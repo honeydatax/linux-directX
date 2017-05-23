86e29508862ec1fa7b7872456e2315f52afd910d
honey data <honeydatax@sapo.pt>
2017-05-23 05:44:47 +0000

    free code direct draw direct x

---
+++ circle.c
@@ -0,0 +1,35 @@
+#include "directX.h"
+
+
+
+
+
+int main()
+{
+    int fbfd = startX();
+char c[]="im love marina.";
+   
+   
+    if (fbfd == -1) exit(1);
+    if ((int)fbp == -1)exit(1);
+
+ball(vinfo.xres/2,vinfo.yres/2,vinfo.xres/12,0,0,255);
+
+
+
+
+
+refresh(); 
+endX(fbfd);
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
