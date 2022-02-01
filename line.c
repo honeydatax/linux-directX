250ac1b57bb78b837cc6b531d54d5f0c068092fe
lisbon <16432726+honeydatax@users.noreply.github.com>
2022-02-01 17:47:42 +0000

    Gcc -o line line.c

--- line.c
+++ line.c
@@ -1,25 +1,18 @@
 #include "directX.h"
-
-
-
-
-
 int main(int argc, char ** argv)
 {
     int fbfd = startX(argv[0]);
-char c[]="im love marina.";
-   
-   
+char c[]="hello world.";
+ 
     if (fbfd == -1) exit(1);
     if ((int)fbp == -1)exit(1);
+for(int x=0;x<(vinfo.xres-1)-20;x=x+20)line(x,0,x,vinfo.yres-1,0,0,255);
+for(int y=0;y<(vinfo.yres-1)-20;y=y+20)line(0,y,vinfo.xres-1,y,0,0,255);
+line(0,0,vinfo.xres-1,vinfo.yres-1,0,0,255);
+line(0,vinfo.yres-1,vinfo.xres-1,0,0,0,255);
 
-//ball(vinfo.xres/2,vinfo.yres/2,vinfo.xres/12,0,0,255);
-for(int x=0;x<vinfo.xres-20;x=x+20)line(x,0,x,vinfo.yres,0,0,255);
-for(int y=0;y<vinfo.yres-20;y=y+20)line(0,y,vinfo.xres,y,0,0,255);
-line(0,0,vinfo.xres,vinfo.yres,0,0,255);
-line(0,vinfo.yres,vinfo.xres,0,0,0,255);
-
-refresh(); 
+//refresh(); 
+sleep(3);
 endX(fbfd);
     return 0;
 }
