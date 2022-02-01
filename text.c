f6bccf58e2882d0bb990c051133fe3c63f81f674
lisbon <16432726+honeydatax@users.noreply.github.com>
2022-02-01 18:08:25 +0000

    Add files via upload

--- text.c
+++ text.c
@@ -1,19 +1,16 @@
 #include "directX.h"
-
-
-
-
-
+//g++ -o text text.c
 int main(int argc, char ** argv)
 {
     int fbfd = startX(argv[0]);
-char c[]="im love marina.";
+	int n;
+char c[]="hello world.";
    
    
     if (fbfd == -1) exit(1);
     if ((int)fbp == -1)exit(1);
 
-
+for(n=0;n<vinfo.xres-1;n++)vline(n,0,vinfo.yres,100,100,255);
 for (int xy=0;xy<vinfo.yres-20;xy=xy+20)gputs(xy,xy,0,0,255,c); 
 
 
