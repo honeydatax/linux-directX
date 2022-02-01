5f484e8ae0d9f45585265f76883c9114480ec81a
lisbon <16432726+honeydatax@users.noreply.github.com>
2022-02-01 17:34:37 +0000

    G++ -o hline hline.c

--- vline.c
+++ vline.c
@@ -2,10 +2,11 @@
 //g++ -o vline vline.c
 int main(int argc,char *argv[])
 {
+    int n=0;
     int fbfd = startX(argv[0]);
     if (fbfd == -1) exit(1);
     if ((int)fbp == -1)exit(1);
-	vline(vinfo.xres/2,0,vinfo.yres,0,0,255);
+	for(n=0;n<vinfo.xres-1;n++)vline(n,0,vinfo.yres,0,0,255);
 	sleep(3);
 	endX(fbfd);
     return 0;
