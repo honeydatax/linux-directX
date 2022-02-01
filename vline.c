ab623d8b40530c62d9fe1472ac2451b564eec137
lisbon <16432726+honeydatax@users.noreply.github.com>
2022-02-01 16:58:13 +0000

    G++ -o vline vline.c

--- vline.c
+++ vline.c
@@ -1,25 +1,13 @@
 #include "directX.h"
-
-
-
-
-
-int main()
+//g++ -o vline vline.c
+int main(int argc,char *argv[])
 {
-    int fbfd = startX();
-
-   
-   
+    int fbfd = startX(argv[0]);
     if (fbfd == -1) exit(1);
     if ((int)fbp == -1)exit(1);
-
-
-vline(vinfo.xres/2,0,vinfo.yres,0,0,255);
-
-
-
- 
-endX(fbfd);
+	vline(vinfo.xres/2,0,vinfo.yres,0,0,255);
+	sleep(3);
+	endX(fbfd);
     return 0;
 }
 
