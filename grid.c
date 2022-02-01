fee1dd7ebe2ca9da5c3baf7123dd30d31b8c76ba
lisbon <16432726+honeydatax@users.noreply.github.com>
2022-02-01 18:18:07 +0000

    Gcc -o grid grid.c

--- grid.c
+++ grid.c
@@ -1,13 +1,11 @@
 #include "directX.h"
-
-
-
-
-
+//g++ -o grid grid.c
 int main(int argc, char ** argv)
+
 {
     int fbfd = startX(argv[0]);
-char c[]="im love marina.";
+char c[]="hello world.";
+int n;
 control cc;
 control *ccc;
 
@@ -19,7 +17,7 @@ cc.h=vinfo.yres;
     if (fbfd == -1) exit(1);
     if ((int)fbp == -1)exit(1);
 
-
+for(n=0;n<vinfo.xres-1;n++)vline(n,0,vinfo.yres,100,100,255);
 grid(cc,5,0,0,255);
 sleep(5);
 
